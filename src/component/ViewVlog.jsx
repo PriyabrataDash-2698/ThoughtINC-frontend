84f9920b1fd33f1905db30ea47dfeeb40b241811
priyabratadash <pbd2698@gmail.com>
2026-04-24 23:42:31 +0530

    WIP

--- src/component/ViewVlog.jsx
+++ src/component/ViewVlog.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ViewVlog = () => {
   return (
-    <div>ViewVlog Coming Soon...</div>
+    <div>ViewVlog Coming Soon...
+      <div>hiii</div>
+    </div>
   )
 }
 
