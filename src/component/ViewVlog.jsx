32d464faac2d521f5f2e988496499bf96dbef0f0
priyabratadash <pbd2698@gmail.com>
2026-04-25 23:59:21 +0530

    WIP

--- src/component/ViewVlog.jsx
+++ src/component/ViewVlog.jsx
@@ -98,42 +98,29 @@ useEffect(() => {
 }, []);
 return(
   <div>
-    {/* {vlogs.map((item)=>item.heading)} */}
-
-
-
     <div className="bg-gray-900 py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">From the blog</h2>
-          <p className="mt-2 text-lg/8 text-gray-300">Learn how to grow your business with our expert advice.</p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
+          {vlogs.map((post) => (
             <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
               <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.datetime} className="text-gray-400">
-                  {post.date}
-                </time>
-                <a
-                  href={post.category.href}
-                  className="relative z-10 rounded-full bg-gray-800/60 px-3 py-1.5 font-medium text-gray-300 hover:bg-gray-800"
-                >
-                  {post.category.title}
-                </a>
+                <div className='text-gray-400'>{post.uploadDate}</div>
               </div>
               <div className="group relative grow">
                 <h3 className="mt-3 text-lg/6 font-semibold text-white group-hover:text-gray-300">
-                  <a href={post.href}>
+                  <a href="#">
                     <span className="absolute inset-0" />
-                    {post.title}
+                    {post.heading}
                   </a>
                 </h3>
                 <p className="mt-5 line-clamp-3 text-sm/6 text-gray-400">{post.description}</p>
               </div>
               <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
-                <img alt="" src={post.author.imageUrl} className="size-10 rounded-full bg-gray-800" />
-                <div className="text-sm/6">
+                <img alt="" src={post.uploadImage} className="size-10 rounded-full bg-gray-800" />
+                {/* <div className="text-sm/6">
                   <p className="font-semibold text-white">
                     <a href={post.author.href}>
                       <span className="absolute inset-0" />
@@ -141,15 +128,13 @@ return(
                     </a>
                   </p>
                   <p className="text-gray-400">{post.author.role}</p>
-                </div>
+                </div> */}
               </div>
             </article>
           ))}
         </div>
       </div>
     </div>
-
-
   </div>
 )
 }
