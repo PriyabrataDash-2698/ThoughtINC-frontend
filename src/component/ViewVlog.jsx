a6b7297fd35fa21b6b2de80622151ff6f7adf262
priyabratadash <pbd2698@gmail.com>
2026-05-20 23:49:18 +0530

    WIP

--- src/component/ViewVlog.jsx
+++ src/component/ViewVlog.jsx
@@ -73,8 +73,12 @@ const ViewVlog = ({userrole }) => {
                 <span className='text-2xl text-amber-300 font font-stretch-75% mx-4'>Thoughts</span>Here...</h2>
             }
           </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {vlogs
+          <div className={vlogs.length === 0 ?"mt-4":"mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"}>
+            {
+              vlogs.length === 0 ? 
+              ( <div className='text-2xl font-bold'>No {status.toLowerCase()} vlogs found</div>):
+            (
+            vlogs
             .map((post) => (
               <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
                 <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
@@ -122,7 +126,8 @@ const ViewVlog = ({userrole }) => {
                   </div>
                 }
               </article>
-            ))}
+            )))
+          }
           </div>
           
         </div>
