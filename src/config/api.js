01d56fbf6ac3781986801b7b868707e89990a244
priyabratadash <pbd2698@gmail.com>
2026-05-27 22:26:07 +0530

    WIP_deployment

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-const API_BASE_URL= "http://localhost:8080"
+const API_BASE_URL= "https://demo-deployment-latest-2-uus4.onrender.com"
 
 export const api = axios.create({
     baseURL:API_BASE_URL
