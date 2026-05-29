ad8914484774bd506a42fb67ac18d67c381e38b4
priyabratadash <pbd2698@gmail.com>
2026-05-29 23:42:57 +0530

    text_changes

--- src/component/IndividualVlog.jsx
+++ src/component/IndividualVlog.jsx
@@ -36,12 +36,12 @@ const IndividualVlog = () => {
     <div>
         <div className='my-10 mx-5'>
             <div className='mx-5 my-5'>
-                <h2 className='text-4xl font-semibold tracking-tight text-pretty wrap-break-word'>{vlogdata?.heading}</h2>
+                <h2 className='lg:text-4xl font-semibold tracking-tight text-pretty wrap-break-word'>{vlogdata?.heading}</h2>
             </div>
             <div className='w-full h-auto object-cover flex justify-center'>
             <img src={vlogdata?.uploadImage} alt="" />
             </div>
-            <div className='text-xl font-semibold mx-5 my-5'>
+            <div className='lg:text-xl font-semibold mx-5 my-5'>
                 <p>{vlogdata?.description}</p>
             </div>
         </div>
